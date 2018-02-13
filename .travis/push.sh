bcd81e63c8f2a166f89b9b41600acd0b00bd6c64
pstickney <770883+pstickney@users.noreply.github.com>
2018-02-13 15:57:46 -0500

    update travis references to name change

--- .travis/push.sh
+++ .travis/push.sh
@@ -2,7 +2,7 @@
 git config --local user.name $GH_USER_NAME
 git config --local user.email $GH_USER_EMAIL
 
-git remote set-url origin https://$GITHUB_API_KEY@github.com/pstickne/wol-java.git
+git remote set-url origin https://$GITHUB_API_KEY@github.com/pstickney/wol-java.git
 git checkout development
 
 mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} versions:commit
